3e2019bdf3a8910759bf69e117c5f9df66186aea
Sofbog <Devilmaycrytrav1@gmail.com>
2023-02-19 19:47:55 +0200

    py lint fix

--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+"""
+This module contains the main function of the program that greets the user.
+"""
 def print_hi(name):
+    """
+        Greets the user by printing 'Hi' followed by the given name.
+
+        Args:
+            name (str): The name of the user to greet.
+        """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
